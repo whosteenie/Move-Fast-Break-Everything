71437f7825a3af77c4da7fc9b207e505a152ba6c
Andres Naranjo <139494079+AndresN23@users.noreply.github.com>
2026-04-10 21:17:24 -0700

    Demo

---
+++ Assets/Scripts(delete after testing)/WASD.cs
@@ -0,0 +1,15 @@
+
+public class WASD : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/TestMovement.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class TestMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+
+    void Update()
+    {
+
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + (movement * moveSpeed) * Time.fixedDeltaTime);
+    }
+}
