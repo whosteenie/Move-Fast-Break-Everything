f1d2cf9901bbbfc7bbdb5c22b9f752ea063e425c
Justin <justinmnmj@gmail.com>
2026-04-20 04:32:34 -0700

    xp orbs and level indicator
    bar at the bottom of the screen to indicate level progress and three tiers of xp orb (values and presentation can be tweaked later)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,17 +3,25 @@ using UnityEngine.UIElements;
 
 public class GameManager : MonoBehaviour {
     [SerializeField] private UIDocument uiDocument;
+    [SerializeField] private int startingXpToNextLevel = 5;
+    [SerializeField] private int xpRequirementIncreasePerLevel = 3;
 
     private const string RunTimerLabelName = "run-timer-label";
+    private const string LevelProgressFillName = "level-progress-fill";
 
     private Label _runTimerLabel;
+    private VisualElement _levelProgressFill;
     private float _currentRunTime;
+    private int _currentLevel = 1;
+    private int _currentXp;
+    private int _xpToNextLevel;
 
     public static GameManager Instance { get; private set; }
     public static float CurrentRunTimeSeconds => Instance != null ? Instance._currentRunTime : 0f;
 
     private void Awake() {
         Instance = this;
+        _xpToNextLevel = startingXpToNextLevel;
 
         if(uiDocument == null) {
             uiDocument = GetComponent<UIDocument>();
@@ -28,8 +36,13 @@ public class GameManager : MonoBehaviour {
 
     private void Start() {
         if(uiDocument == null) return;
-        _runTimerLabel = uiDocument.rootVisualElement.Q<Label>(RunTimerLabelName);
+
+        var root = uiDocument.rootVisualElement;
+        _runTimerLabel = root.Q<Label>(RunTimerLabelName);
+        _levelProgressFill = root.Q<VisualElement>(LevelProgressFillName);
+
         _runTimerLabel.text = FormatRunTime(_currentRunTime);
+        RefreshLevelProgressBar();
     }
 
     private void Update() {
@@ -38,6 +51,41 @@ public class GameManager : MonoBehaviour {
         if(_runTimerLabel != null) {
             _runTimerLabel.text = FormatRunTime(_currentRunTime);
         }
+    }
+
+    public void AddExperience(int amount)
+    {
+        if (amount <= 0)
+        {
+            return;
+        }
+
+        _currentXp += amount;
+
+        while (_currentXp >= _xpToNextLevel)
+        {
+            _currentXp -= _xpToNextLevel;
+            _currentLevel++;
+            _xpToNextLevel = GetXpRequiredForLevel(_currentLevel);
+        }
+
+        RefreshLevelProgressBar();
+    }
+
+    private void RefreshLevelProgressBar()
+    {
+        if (_levelProgressFill == null)
+        {
+            return;
+        }
+
+        var progress = _xpToNextLevel > 0 ? (float)_currentXp / _xpToNextLevel : 0f;
+        _levelProgressFill.style.width = Length.Percent(progress * 100f);
+    }
+
+    private int GetXpRequiredForLevel(int level)
+    {
+        return startingXpToNextLevel + (level - 1) * xpRequirementIncreasePerLevel;
     }
 
     private static string FormatRunTime(float runTimeSeconds) {
