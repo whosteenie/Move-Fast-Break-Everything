4c6bad29835ffb0d18cc63bf3d45d9cb7dd0d6b1
Justin <justinmnmj@gmail.com>
2026-04-20 03:45:02 -0700

    initial main menu

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,16 +1,45 @@
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
+[RequireComponent(typeof(UIDocument))]
 public class MainMenuManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private string playSceneName = "GameScene";
+
+    public event Action<string> ButtonPressed;
+
+    private void Start()
     {
-        
+        var root = GetComponent<UIDocument>().rootVisualElement;
+
+        root.Q<Button>("play-button").clicked += () => HandleButtonPressed("play", PlayGame);
+        root.Q<Button>("options-button").clicked += () => HandleButtonPressed("options", OpenOptions);
+        root.Q<Button>("quit-button").clicked += () => HandleButtonPressed("quit", QuitGame);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void HandleButtonPressed(string buttonId, Action action)
     {
-        
+        ButtonPressed?.Invoke(buttonId);
+        action();
+    }
+
+    private void PlayGame()
+    {
+        SceneManager.LoadScene(playSceneName);
+    }
+
+    private void OpenOptions()
+    {
+    }
+
+    private void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }
