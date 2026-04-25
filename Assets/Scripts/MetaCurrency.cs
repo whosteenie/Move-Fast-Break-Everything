d4be71aa4823eb40ed6f707f09de435c38bb2617
Justin <justinmnmj@gmail.com>
2026-04-24 20:32:48 -0700

    initial logic for dropped items
    we can likely simplify enemy and xp orbs by removing shared code from these generic scripts for some functionality

---
+++ Assets/Scripts/MetaCurrency.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public static class MetaCurrency
+{
+    private const string TotalCoinsKey = "MetaCurrency.TotalCoins";
+
+    public static int TotalCoins => PlayerPrefs.GetInt(TotalCoinsKey, 0);
+
+    public static void AddCoins(int amount)
+    {
+        if (amount <= 0)
+        {
+            return;
+        }
+
+        var newTotal = TotalCoins + amount;
+        PlayerPrefs.SetInt(TotalCoinsKey, newTotal);
+        PlayerPrefs.Save();
+        Debug.Log($"Added {amount} coins. Total coins: {newTotal}");
+    }
+}
---
+++ Assets/Scripts/Pickups/CoinPickup.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class CoinPickup : MagneticPickup
+{
+    [SerializeField] private int minCoinValue = 1;
+    [SerializeField] private int maxCoinValue = 3;
+
+    protected override bool TryCollect(GameObject playerObject)
+    {
+        var minValue = Mathf.Max(0, minCoinValue);
+        var maxValue = Mathf.Max(minValue, maxCoinValue);
+        var coinAmount = Random.Range(minValue, maxValue + 1);
+
+        if (coinAmount <= 0)
+        {
+            return false;
+        }
+
+        MetaCurrency.AddCoins(coinAmount);
+        return true;
+    }
+}
